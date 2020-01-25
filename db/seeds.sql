f230b0f13c8c49d98dd319de079a2f4595480021
JHabte <j.habtema@gmail.com>
2020-01-25 18:52:36 -0500

    initialied server.js, node packages,db schema

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false); 
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
